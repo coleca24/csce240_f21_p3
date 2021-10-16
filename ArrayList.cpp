339490c1a5eb3b35ab4a8bc5b352a8747f00823e
coleca24 <coleca@email.sc.edu>
2021-10-16 14:56:44 -0400

    Create ArrayList.cpp

---
+++ ArrayList.cpp
@@ -0,0 +1,102 @@
+include "./ArrayList.h"
+  
+template <class U>
+ostream& operator<<(ostream & os, ArrayList<U> a) {
+    for (int i = 0; i < a.getSize(); i++) {
+        os << a.getData(i) << " ";
+    }
+    os << "\n";
+    return os;
+}
+
+template <class T>
+ArrayList<T>::ArrayList() {
+    size = 0;
+    data = new int[size];
+}
+
+template <class T>
+ArrayList<T>::ArrayList(int size, int val) {
+    this->size = size;
+    data = new int[size];
+    for (int i = 0; i <size; i++) {
+        data[i] = val;
+    }
+}
+
+template <class T>
+ArrayList<T>::ArrayList(const ArrayList<T> & copy) {
+    setSize(copy.getSize());
+    data = new int[size];
+    for (int i = 0; i < size; i++) {
+        data[i] = copy[i];
+    }
+}
+
+template <class T>
+ArrayList<T>::~ArrayList() {
+    delete [] data;
+}
+
+template <class T>
+void ArrayList<T>::setSize(int size) {
+    if (size >= 0) {
+        this->size = size;
+    } else {
+        std::cout << "Incorrect size\n";
+        exit(1);
+    }
+}
+
+template <class T>
+int ArrayList<T>::getSize() const {
+    return size;
+}
+
+template <class T>
+int ArrayList<T>::getData(int index) const {
+    if (index >= 0 && index < size) {
+        return data[index];
+    } else {
+        std::cout << "Invalid Index\n";
+        exit(0);
+    }
+}
+
+template <class T>
+int ArrayList<T>::operator[](int index) const {
+    if (index >= 0 && index < size) {
+        return data[index];
+    } else {
+        std::cout << "Invalid Index\n";
+        exit(0);
+    }
+}
+
+template <class T>
+int& ArrayList<T>::operator[](int index) {
+    return data[index];
+}
+
+template <class T>
+const ArrayList<T> & ArrayList<T>::operator=(const ArrayList<T> & rhs) {
+    size = rhs.getSize();
+    delete [] data;
+    data = new int[size];
+    for (int i = 0; i < size; i++) {
+        data[i] = rhs[i];
+    }
+    return *this;
+}
+
+template <class T>
+void ArrayList<T>::print() const {
+    for (int i = 0; i < size; i++) {
+        std::cout << data[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+// Start of new functions
+
+
