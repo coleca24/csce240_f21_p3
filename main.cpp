d972dfdaa2654a316a9989a47a8e42bb2a5fe314
coleca24 <coleca@email.sc.edu>
2021-10-17 11:56:12 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 #include "./ArrayList.cpp"
 
 int main(int argc, char **argv) {
+  ArrayList<int> a(2,1), b(3,1);
+  // ------------Testing Operator!=-----------------
+  std::cout << "----\nTesting operator!= \n";
+  std::cout << "Your output: \n";
+  bool test = (a != b); 
+  std::cout << test << "\n"; 
+  std::cout << "Correct output: \ntrue\n";
+  
+  std::cout << "----\nTesting operator!= \n";
+  std::cout << "Your output: \n";
+  bool test = (a != a); 
+  std::cout << test << "\n"; 
+  std::cout << "Correct output: \nfalse\n";
+  
+  
   return 0; 
 }
